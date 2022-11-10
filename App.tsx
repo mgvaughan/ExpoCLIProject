02a7737c2538d52f1654ba88e0ea3d55c10b3127
Michael Vaughan <mgvaughan66@gmail.com>
2022-11-10 16:46:42 -0500

    starting and finishing Expo-CLI lab

--- App.tsx
+++ App.tsx
@@ -3,18 +3,38 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container1}>
+      <View style={styles.container2}>
+        <View style={styles.container3}>
+          <Text style={styles.textStyle}>#crushingit</Text>
+          <StatusBar style="auto" />
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container1: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
+    padding: 20,
+  },
+  container2: {
+    flex: 1,
+    backgroundColor: 'black',
+    padding: 20,
+  },
+  container3: {
+    flex: 1,
+    backgroundColor: 'lightgreen',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    backgroundColor: 'red',
+    color: 'white',
+    fontSize: 40,
+    fontFamily: 'Optima-Regular'
+  }
 });
